0db7e62bf910ea541a82aff8c7cc270e4dc49ba1
Hritik <hritikkanojiya13@gmail.com>
2023-06-08 07:57:36 +0530

    Resolved MongoDB Error

--- helpers/common/environment.js
+++ helpers/common/environment.js
@@ -1,19 +1,16 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const mongoConfig = {
-    MONGO_USER_NAME:process.env.MONGODB_USER,
-    MONGO_PASSWORD:process.env.MONGODB_PASS,
-    MONGO_HOST:process.env.MONGODB_DB_HOST,
-    MONGO_PORT:process.env.MONGODB_DB_PORT,
-    MONGO_DB_NAME:process.env.MONGODB_DB_NAME,
+  MONGO_USER_NAME: process.env.MONGODB_USER,
+  MONGO_PASSWORD: process.env.MONGODB_PASS,
+  MONGO_HOST: process.env.MONGODB_DB_HOST,
+  MONGO_PORT: process.env.MONGODB_DB_PORT,
+  MONGO_DB_NAME: process.env.MONGODB_DB_NAME,
+};
 
-
-}
-
-const SECRET_KEY = process.env.SECRET_KEY
-
+const SECRET_KEY = process.env.SECRET_KEY;
 
 module.exports = {
-    mongoConfig,
-    SECRET_KEY
-}
+  mongoConfig,
+  SECRET_KEY,
+};
--- helpers/common/init_monodb.js
+++ helpers/common/init_monodb.js
@@ -1,27 +1,36 @@
-const mongoose = require('mongoose');
-const { mongoConfig } = require('./environment');
-const {
-    logBackendError,
-} = require("./backend.functions");
+const mongoose = require("mongoose");
+const { mongoConfig } = require("./environment");
+const { logBackendError } = require("./backend.functions");
 
-const mongoURI = `mongodb://${mongoConfig.MONGO_USER_NAME}:${mongoConfig.MONGO_PASSWORD}@${mongoConfig.MONGO_HOST}:${mongoConfig.MONGO_PORT}/${mongoConfig.MONGO_DB_NAME}`;
+// const mongoURI = `mongodb://${mongoConfig.MONGO_USER_NAME}:${mongoConfig.MONGO_PASSWORD}@${mongoConfig.MONGO_HOST}:${mongoConfig.MONGO_PORT}/${mongoConfig.MONGO_DB_NAME}`;
+
+let mongodbURI =
+  `mongodb${process.env.NODE_ENV === "production" ? "+srv://" : "://"}` +
+  process.env.MONGODB_DB_HOST;
+
+if (process.env.NODE_ENV === "development") {
+  mongodbURI = `${mongodbURI}:${process.env.MONGODB_DB_PORT}`;
+}
 
 const connectToMongoDB = () => {
-    mongoose
-        .connect(mongoURI, {})
-        .then(() => {
-            console.log('Connected to MongoDB');
-        })
-        .catch((err) => {
-            console.error('Error connecting to MongoDB', err);
-            logBackendError(
-                __filename,
-                err?.message,
-                null,
-                null,
-                "Unable to start MongoDB."
-            );
-        });
+  // Establish Connection to MongoDB Server
+  mongoose.set("strictQuery", false);
+  mongoose
+    .connect(mongodbURI, {
+      dbName: process.env.MONGODB_DB_NAME,
+      user: process.env.MONGODB_USER,
+      pass: process.env.MONGODB_PASS,
+      useNewUrlParser: true,
+      useUnifiedtopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      logBackendError(__filename, error?.message, null, null, error?.stack);
+      console.error("Application could not connect to MongoDB Server");
+      process.exit(0);
+    });
 };
 
 module.exports = connectToMongoDB;
