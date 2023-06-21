75718db119f4766989e9d8a15c9f73138a04c683
RENOLD <developer@verandalearning.com>
2023-06-21 15:39:12 +0530

    break

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,36 +2,108 @@ const moment = require('moment');
 const activity = require('../models/activity');
 
 const attendenceMarker = function (req, res) {
-
   const currentDate = moment().format('DD-MM-YYYY');
-
   let promise = new Promise(async (resolve, reject) => {
-    await activity.findOne({ email: req.body.email, date: currentDate })
-      .then(async (findUser) => {
-        if (findUser) {
-          let activities = findUser.activities
+    try {
+      const findUser = await activity.findOne({ email: req.body.email, date: currentDate });
 
-          if (activities[activities.length - 1].activity == req.body.activity) {
-            resolve({ message: `already ${req.body.activity} activity is done` })
-          }
-          else {
+      if (findUser) {
+        let activities = findUser.activities;
+        let latestActivity = activities[activities.length - 1].activity;
+
+        if (req.body.activity === 'breakin') {
+          // Check if the latest activity is checkin or checkout
+          if (latestActivity === 'checkin' || latestActivity === 'breakout') {
+            // Update the activities array with the new activity and time
+            let updatedActivities = [...activities];
+            updatedActivities.push({
+              activity: req.body.activity,
+              time: moment().format('HH:mm:ss'),
+            });
+
+            // Update the user's activities
             await activity.updateOne(
               { email: req.body.email, date: currentDate },
               {
-                $push: {
-                  activities: {
-                    activity: req.body.activity,
-                    time: moment().format('HH:mm:ss'),
-                  }
-                },
+                $set: { activities: updatedActivities },
               }
             );
+
+            resolve({ message: `${req.body.activity} marked successfully` });
+          } else {
+            resolve({ message: "Can't perform this activity without checking in or break out" });
           }
+        } else if (req.body.activity === 'checkin') {
+          // Check if the latest activity is checkout or checkin
+          if (latestActivity === 'checkout' ) {
+            // Update the activities array with the new activity and time
+            let updatedActivities = [...activities];
+            updatedActivities.push({
+              activity: req.body.activity,
+              time: moment().format('HH:mm:ss'),
+            });
+
+            // Update the user's activities
+            await activity.updateOne(
+              { email: req.body.email, date: currentDate },
+              {
+                $set: { activities: updatedActivities },
+              }
+            );
+
+            resolve({ message: `${req.body.activity} marked successfully` });
+          } else {
+            resolve({ message: "Can't perform this activity without checking out" });
+          }
+        } else if (req.body.activity === 'checkout') {
+          // Check if the latest activity is checkin
+          if (latestActivity === 'checkin' || latestActivity === 'breakout') {
+            // Update the activities array with the new activity and time
+            let updatedActivities = [...activities];
+            updatedActivities.push({
+              activity: req.body.activity,
+              time: moment().format('HH:mm:ss'),
+            });
+
+            // Update the user's activities
+            await activity.updateOne(
+              { email: req.body.email, date: currentDate },
+              {
+                $set: { activities: updatedActivities },
+              }
+            );
+
+            resolve({ message: `${req.body.activity} marked successfully` });
+          } else {
+            resolve({ message: "Can't perform this activity without checking in" });
+          }
+        } else if(req.body.activity === 'breakout') {
+          if (latestActivity === 'breakin') {
+          // Update the activities array with the new activity and time
+          let updatedActivities = [...activities];
+          updatedActivities.push({
+            activity: req.body.activity,
+            time: moment().format('HH:mm:ss'),
+          });
+          // Update the user's activities
+          await activity.updateOne(
+            { email: req.body.email, date: currentDate },
+            {
+              $set: { activities: updatedActivities },
+            }
+          );
+          
+          resolve({ message: `${req.body.activity} marked successfully` });
+          }
+          else {
+            resolve({ message: "Can't perform this activity without breakin break" });
+          }
+        }
+      } else {
+        if (req.body.activity === 'checkout' || req.body.activity === 'breakin' || req.body.activity === 'breakout') {
+          resolve({ message: "Can't perform this activity without checking in" });
         } else {
-          if(req.body.activity=="Checkout"){
-            resolve({message:"Can't checkout before checkin"})
-          }
-          else{
+          // Create a new attendance record for the user
           await activity.create({
             email: req.body.email,
             fullname: req.body.fullname,
@@ -43,14 +115,13 @@ const attendenceMarker = function (req, res) {
             ],
             date: currentDate,
           });
+
+          resolve({ message: `${req.body.activity} marked successfully` });
         }
-        }
-        let message = req.body.activity == 'Checkin' ? "Checked in succesfully" : "Checked out Successfully"
-        resolve({ message: `${message}` });
-      })
-      .catch((error) => {
-        reject(error);
-      });
+      }
+    } catch (error) {
+      reject(error);
+    }
   });
 
   promise
@@ -62,9 +133,13 @@ const attendenceMarker = function (req, res) {
     });
 };
 
+
+
+
+
+
 const getUserActivity = function (req, res) {
   const currentDate = moment().format('DD-MM-YYYY');
-
   let promise = new Promise((resolve, reject) => {
     activity.findOne({ email: req.body.email, date: currentDate })
       .then(async (findUser) => {
@@ -77,14 +152,11 @@ const getUserActivity = function (req, res) {
         } else {
           resolve(false);
         }
-
-
       })
       .catch((error) => {
         reject(error);
       });
   });
-
   promise
     .then((data) => {
       res.status(200).send({ code: 200, data: data });
